f11a67fa1e909e307107e66b0f36f69a9574e852
Nicholas Shephard <shephardn@gmail.com>
2021-12-19 20:12:21 -0600

    second commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,31 @@
 class Movie < ActiveRecord::Base
     
-    def create_with_title(given_title)
-        flick = Movie.new(title: given_title)
-        flick.save
+    def self.create_with_title(given_title)
+        flick = Movie.create(title: given_title)
     end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(n)
+        Movie.find(n)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes);
+    end
+
+    def self.find_movies_after_2002
+        Movie.where(release_date > 2002)
+    end
+
 end
